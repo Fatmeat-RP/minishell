e63cb58a53ac12da6f6f7c9638999fe4717d6703
Alexandre Carle-Margueritte <acarle-m@student.42lyon.fr>
2022-07-24 06:07:57 +0200

    wip

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -2,7 +2,8 @@
 
 static int	looper(char **arg)
 {
-	int	i;
+	int		i;
+	char	*line;
 
 	i = 0;
 	while (arg[1] && arg[1][i] != '=')
@@ -18,9 +19,18 @@ int	builtin_export(char **arg, char **envp)
 {
 	char	*line;
 	int		i;
+	size_t	size;
 
 	i = 0;
-	line = malloc(looper(arg[1]));
-	while ()
+	/* WIP not working, don't use or try it, need to work on it*/
+	while(arg[j] != NULL)
+	{
+		line = looper(arg);
+		size = ft_strlen(line);
+		while (envp[i] && ft_strncmp(envp[i], line, size) != 0)
+			i++;
+		envp[i] = ft_strdup(arg[1]);
+		free(line);
+	}
 	return (0);
 }
